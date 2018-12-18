6ce516eef8e817dd707af3f3f4f68eaef63c4264
QuidObscurum <quid.obscurum.quid.divinum@gmail.com>
2018-12-19 01:19:07 +0300

    Add css

--- games/views.py
+++ games/views.py
@@ -5,6 +5,7 @@ from . import form
 from django.template.context_processors import csrf
 from django.contrib import auth
 from django.contrib.auth import get_user_model, login, logout
+from django.db.models import Count
 
 User = get_user_model()
 
@@ -15,21 +16,26 @@ def show(request):
     content = []
     for v in Video.objects.all():
         tmp = []
-        tmp.append(v)
-        comments = Comment.objects.filter(Comment_video_id=v.id)
+        l_videos = []
+        l_videos.append(v)
+        l_videos.append(len(v.Video_likers.all()))
+        tmp.append(l_videos)
+        # comments = Comment.objects.filter(Comment_video_id=v.id)
+        comments_set = Comment.objects.filter(Comment_video_id=v.id) \
+            .annotate(likes_count=Count('Comment_likers')) \
+            .order_by('-likes_count')[:2]
         l_users = []
         l_comment_likes = []
-        for com in comments:
+        for com in comments_set:
             comment_likes_len = len(com.Comment_likers.all())
             l_comment_likes.append(comment_likes_len)
             l_users.append(User.objects.get(id=com.Comment_user_id))
-        tmp.append(list(zip(comments, l_users, l_comment_likes)))
+        tmp.append(list(zip(comments_set, l_users, l_comment_likes)))
 
         content.append(tmp)
     # <content>[
     #   <tmp>[
-    #       Video, <comms>[
-    #           (Comment, User),(Comment, User)...]]]
+    #       <videos>[video, Likes], <comms>[(Comment, User, Likes),(Comment, User, Likes)...]]]
     return render(request, 'dj_template.html', {'content': content, 'user': auth.get_user(request).username})
     # return render(request, 'dj_template.html', {'name': 'Diana'})
     # return HttpResponse('Hello')
@@ -39,8 +45,9 @@ def showOneVideo(request, video_id):
     kargs = {}
     kargs.update(csrf(request))  # TOKEN
     kargs['form'] = form.CommentForm
-
-    kargs['video'] = Video.objects.get(id=video_id)
+    video = Video.objects.get(id=video_id)
+    kargs['video'] = video
+    kargs['video_likes'] = len(video.Video_likers.all())
     kargs['user'] = auth.get_user(request).username
     comments = Comment.objects.filter(Comment_video_id=video_id)
     Users_list = []
@@ -114,28 +121,33 @@ def goin(request):
         return render(request, 'sign.html', kargs)
 
 
-def likevideo(request, video_id):
-    video = Video.objects.get(id=video_id)
-    # kargs['user'] = User.objects.get(id=request.user.id)
-    video.Video_likes += 1
-    video.save()
-    return redirect("/games/showOne/" + str(video_id) + "/")
+# def likevideo(request, video_id):
+#     video = Video.objects.get(id=video_id)
+#     # kargs['user'] = User.objects.get(id=request.user.id)
+#     video.Video_likes += 1
+#     video.save()
+#     return redirect("/games/showOne/" + str(video_id) + "/")
 
-def likecomment(request, comment_id):
-    comment = Comment.objects.get(id=comment_id)
-    # kargs['user'] = User.objects.get(id=request.user.id)
-    comment.Comment_likes += 1
-    comment.save()
-    video_id = comment.Comment_video_id
-    return redirect("/games/showOne/" + str(video_id) + "/")
+# def likecomment(request, comment_id):
+#     comment = Comment.objects.get(id=comment_id)
+#     # kargs['user'] = User.objects.get(id=request.user.id)
+#     comment.Comment_likes += 1
+#     comment.save()
+#     video_id = comment.Comment_video_id
+#     return redirect("/games/showOne/" + str(video_id) + "/")
 
 def ajaxvid(request):
     if request.GET:
         video_id = request.GET['addlike']
         video = Video.objects.get(id=video_id)
-        video.Video_likes += 1
+        liker = User.objects.get(id=request.user.id)
+        # video.Video_likes += 1
+        if liker in video.Video_likers.all():
+            video.Video_likers.remove(liker)
+        else:
+            video.Video_likers.add(liker)
         video.save()
-    return HttpResponse(video.Video_likes)
+    return HttpResponse(len(video.Video_likers.all()))
 
 def ajaxcomm(request):
     if request.GET:
@@ -143,7 +155,10 @@ def ajaxcomm(request):
         comment = Comment.objects.get(id=comment_id)
         # comment.Comment_likes += 1
         liker = User.objects.get(id=request.user.id)
-        comment.Comment_likers.add(liker)
+        if liker in comment.Comment_likers.all():
+            comment.Comment_likers.remove(liker)
+        else:
+            comment.Comment_likers.add(liker)
         comment.save()
     return HttpResponse(len(comment.Comment_likers.all()))
 
